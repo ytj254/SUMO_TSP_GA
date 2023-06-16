4d179ff69cb01171025015aa87c2023fe5614916
ytj254 <56201152+ytj254@users.noreply.github.com>
2023-06-16 09:57:09 -0400

    Add files via upload

---
+++ SUMO_TSP/backup/Base/main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+import numpy as np
+from Analysis import analysis
+
+# we need to import some python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME")
+
+from sumolib import checkBinary  # Check for the binary in environ vars
+import traci
+
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true",
+                          default=False, help="run the commandline version of sumo")
+    arg = ['--nogui']
+    options, args = opt_parser.parse_args(arg)
+    return options
+
+
+#  contains TraCI control loop
+def run():
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+    traci.close()
+    sys.stdout.flush()
+
+
+def test():
+    options = get_options()
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # traci starts sumo as a subprocess and then this script connects and runs
+    # "--step-length", "0.1",
+    traci.start([sumoBinary, "-c", "data/Eastway-Central.sumocfg",
+                 "--tripinfo-output", "tripinfo.xml", "--duration-log.statistics", "--log", "logfile.xml"])
+    run()
+
+
+def iteration():
+    options = get_options()
+    totres = []
+    n = 20
+    for i in range(n):  # run n simulations
+        #  check binary
+        print(i)
+        if options.nogui:
+            sumoBinary = checkBinary('sumo')
+        else:
+            sumoBinary = checkBinary('sumo-gui')
+
+        # traci starts sumo as a subprocess and then this script connects and runs
+        # "--step-length", "0.1",
+        traci.start([sumoBinary, "-c", "data/Eastway-Central.sumocfg", "--seed", "%d" % i,
+                     "--tripinfo-output", "tripinfo.xml",
+                     "--duration-log.statistics", "--log", "logfile.xml"])
+        run()
+        res = analysis("tripinfo.xml")
+        totres.append(res)
+
+    # ares = np.concatenate(totres, axis=0)
+    ares = np.reshape(totres, (n, 16))  # change list to array and reshape
+    np.savetxt('totalresult.csv', ares, delimiter=',')  # save to files
+    # print(ares)
+
+
+# main entry point
+if __name__ == "__main__":
+    iteration()
